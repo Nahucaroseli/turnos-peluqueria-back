6f3d08a1ccbfb1eed1226cd9418a31fa2aa5231b
Nahuel Caroseli <nahucaroseli2100@gmail.com>
2026-02-22 20:38:20 -0300

    agregado estructura backend

---
+++ src/main/java/com/turnospeluqueria/vimix/VimixApplication.java
@@ -0,0 +1,13 @@
+package com.turnospeluqueria.vimix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VimixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VimixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turnospeluqueria/vimix/VimixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turnospeluqueria.vimix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VimixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
