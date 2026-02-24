d06919d537757088b08ad843070a18a222a91c22
Nahuel Caroseli <nahucaroseli2100@gmail.com>
2026-02-24 13:37:35 -0300

    finalizado funcion edit

--- src/main/java/com/turnospeluqueria/vimix/services/ClientService.java
+++ src/main/java/com/turnospeluqueria/vimix/services/ClientService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClientService {
@@ -19,6 +20,17 @@ public class ClientService {
 
     public Client addClient(Client cliente){
         return repo.save(cliente);
+    }
+
+
+    public Client editClient(Long id, Client cliente){
+        Optional<Client> co = repo.findById(id);
+        if(co.isPresent()){
+            if(co.get().getId().equals(cliente.getId())){
+                return repo.save(cliente);
+            }
+        }
+        return null;
     }
 
 }
