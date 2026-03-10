6b7ba559f5906f0e27ee7ef1532d65c1a70766d3
Nahuel Caroseli <nahucaroseli2100@gmail.com>
2026-03-10 13:47:12 -0300

    agregado dto para retornar horarios disponibles en x fecha

---
+++ src/main/java/com/turnospeluqueria/vimix/dto/TurnoDisponibleDTO.java
@@ -0,0 +1,37 @@
+package com.turnospeluqueria.vimix.dto;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TurnoDisponibleDTO {
+
+    private LocalDate fecha;
+
+    private List<LocalTime> horarios;
+
+
+    public TurnoDisponibleDTO() {
+    }
+
+    public void addHorarios(LocalTime h){
+        if(!horarios.contains(h)){
+            horarios.add(h);
+        }
+    }
+
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public List<LocalTime> getHorarios() {
+        return new ArrayList<>(horarios);
+    }
+
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+}
